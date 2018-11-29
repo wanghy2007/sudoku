6c4e4c78d8b0a10358f3ed56a89f3010d614836a
wang_ <wanghy2007@gmail.com>
2018-11-29 11:53:47 -0800

    Initial commit.

---
+++ sudoku/src/main/java/com/moonbase/hobby/sudoku/App.java
@@ -0,0 +1,13 @@
+package com.moonbase.hobby.sudoku;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
